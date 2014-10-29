dc414d94593e2889b22d23211d8ef9128111b79a
Swanand Pagnis <swanand.pagnis@gmail.com>
2014-10-29 22:26:06 +0530

    Ignore RecordNotFound and RoutingError

--- config/initializers/opbeat.rb
+++ config/initializers/opbeat.rb
@@ -4,4 +4,5 @@ Opbeat.configure do |config|
   config.organization_id = ENV['OPBEAT_ORGID']
   config.app_id          = ENV['OPBEAT_APPID']
   config.secret_token    = ENV['OPBEAT_SECRET']
+  config.excluded_exceptions = %w(ActionController::RoutingError ActiveRecord::RecordNotFound)
 end
